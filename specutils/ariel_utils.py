7b917de7bde5ec96edcb3758f0378e361e9f6fb7
Patrick Lavin <plavin997@gmail.com>
2022-03-30 14:30:17 -0400

    add parsing functionality

---
+++ specutils/ariel_utils.py
@@ -0,0 +1,66 @@
+# This module will parse a command and output a dictionary
+# that can be provided to ariel to specify the command you want
+# to run. Specify any number of arguments and redirects.
+
+# For now, we will focus on input redirect. Others many be added later
+
+# TODO: support standard file descriptors
+# TODO: support env params (assignment node)
+
+import bashlex
+def parseAriel (command):
+
+    cmd    = None
+    args   = []
+    stdin  = None
+    stdout = None
+    stderr = None
+    append = False
+
+    parts = bashlex.parse(command)
+
+    if (len(parts) != 1):
+        raise ValueError('Expected to recieve exactly one part from the parser')
+
+    ast = parts[0]
+
+    if (ast.kind != 'command'):
+        raise ValueError('The command should not include multiple parts, such as with && or |')
+
+    for part in ast.parts:
+        if (part.kind == 'word'):
+            if (cmd == None):
+                cmd = part.word
+            else:
+                args.append(part.word)
+        elif (part.kind == 'redirect'):
+            if (part.type == '<'):
+                stdin = part.output.word
+            elif (part.type == '>'):
+                stdout = part.output.word
+                redirect = False
+            elif (part.type == '>>'):
+                stdout = part.output.word
+                append = True
+        else:
+            start = part.pos[0]
+            end = part.pos[1]
+            raise ValueError('Unsupported command part: {} ({})'.format(command[start:end], part.kind))
+
+
+    if (not cmd):
+        raise ValueError('No command detected')
+
+    params = {}
+
+    params['executable'] = cmd
+
+    params['appargcount'] = len(args)
+    for i in range(len(args)):
+        params['apparg{}'.format(i)] = args[i]
+
+    if stdin:
+        params['appstdin'] = stdin
+
+    return params
+
---
+++ specutils/spectest.py
@@ -0,0 +1,45 @@
+import sst
+from ariel_utils import parseAriel
+
+# Utility method
+def mklink(e1, e2):
+      link = sst.Link("link_" + e1[0].getFullName() + "[" + e1[1] + "]" + "_" +
+                                e2[0].getFullName() + "[" + e2[1] + "]" )
+      link.connect(e1, e2)
+      return link
+
+ariel_command = "ls -al"
+
+# Components
+core    = sst.Component("Ariel", "ariel.ariel")
+gen     = core.setSubComponent("generator", "miranda.CopyGenerator")
+l1      = sst.Component("L1Cache", "memHierarchy.Cache")
+memctrl = sst.Component("MemoryController", "memHierarchy.MemController")
+backend = memctrl.setSubComponent("backend", "memHierarchy.simpleMem")
+
+# Parameters
+freq="2GHz"
+core.addParams({"clock":freq})
+core.addParams(parseAriel(ariel_command))
+gen.addParams({"request_count":20})
+l1.addParams({"l1":"true", "cache_frequency":freq, "cache_size":"1KiB", "associativity":4, "access_latency_cycles":2, "L1":"true"})
+memctrl.addParams({"clock":freq, "backing":"none", "addr_range_end":1024**3-1})
+backend.addParams({"mem_size":"1GiB"})
+
+# Links
+latency="1000ps"
+mklink((core, "cache_link_0", latency),  (l1, "high_network_0", latency))
+mklink((l1, "low_network_0", latency), (memctrl, "direct_link", latency))
+
+# Satatistics
+l1.enableStatistics(["GetS_recv","CacheHits", "CacheMisses","TotalEventsReceived","MSHR_occupancy" ])
+core.enableStatistics(["split_read_reqs"])
+
+# Define SST core options
+sst.setProgramOption("timebase", "1ps")
+sst.setStatisticLoadLevel(9)
+#sst.enableAllStatisticsForAllComponents()
+
+sst.setStatisticOutput("sst.statOutputCSV", {"filepath" : "./spectest_stats.csv", "separator" : ", " } )
+
+# stats
